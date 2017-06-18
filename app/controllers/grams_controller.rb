eecf5f1f9f37d1c7234637b67021f30c67954480
Justin Bland <jrbland@yahoo.com>
2017-06-18 21:01:11 +0000

    added tests to keep to only only loggin users edit grams, also only let gram creators  delete grams they created

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
   end
@@ -19,18 +19,19 @@ class GramsController < ApplicationController
   
   def show
     @gram = Gram.find_by_id(params[:id])
-    return render_not_found if  @gram.blank?
+    return render_not_found if @gram.blank?
   end
   
   def edit
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if  @gram.blank?
+    return render_not_found(:forbidden) if current_user != @gram.user
   end
   
   def update
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
-    
+    return render_not_found(:forbidden) if current_user != @gram.user
     @gram.update_attributes(gram_params)
     
     if @gram.valid?
@@ -43,6 +44,8 @@ class GramsController < ApplicationController
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if current_user != @gram.user
+
     @gram.destroy
     redirect_to root_path
   end
@@ -53,7 +56,9 @@ class GramsController < ApplicationController
     params.require(:gram).permit(:message)
   end
   
-  def render_not_found
-    render plain: "Not Found", status: :not_found
+  def render_not_found(status = :not_found)
+    render plain: "#{status.to_s.titleize}", status: status
   end
+  
+  
 end
